006af1c8b965cf74c72812d474d827a34112e343
arnaud <arnaudhj@sfr.fr>
2025-02-08 20:32:31 +0100

    Check if all parameters needed to use the weather feature are defined.

--- UserConfig.cmake
+++ UserConfig.cmake
@@ -8,9 +8,10 @@ set(TRACE_TO_STDIO "0")
 # Configuration of the optional Weather menu.  Requires configuration of OPEN_WEATHER_MAP_URL, WIFI_SSID, and WIFI_PASSWORD below
 set(INCLUDE_WEATHER "0")  # 0 or 1, 1 = Include the Weather Submenu and processing.  See below for configuration notes.   
 
+set (WIFI_SSID "")
+set (WIFI_PASSWORD "")
+
 add_compile_definitions(
-    WIFI_SSID=\"\"
-    WIFI_PASSWORD=\"\"
     UTC_OFFSET=1 # UTC offset as real number of hours. For example, set 5.5 for UTC+05:30.
 
     # Set location for automatic daylight saving time observation. 
